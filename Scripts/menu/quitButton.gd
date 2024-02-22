e28e411e560e345ee2536cdaa962a5c1417894b1
Redokpaw <Redokpaw@gmail.com>
2024-02-22 20:20:13 +0300

    feat: updated menu

--- Scripts/menu/quitButton.gd
+++ Scripts/menu/quitButton.gd
@@ -1,8 +1,12 @@
 extends Button
 
+@onready var defaultModulate = $quit.modulate
+
 func _on_pressed():
 	get_tree().quit()
 	
-func _on_mouse_entered():
-	$Sprite2D.visible = true
-	
+func _on_mouse_entered(): 
+	$quit.modulate = Color.WEB_MAROON
+
+func _on_mouse_exited():
+	$quit.modulate = defaultModulate
--- Scripts/menu/startButton.gd
+++ Scripts/menu/startButton.gd
@@ -1,8 +1,12 @@
 extends Button
 
+@onready var defaultModulate = $start.modulate
+
 func _on_pressed():
 	get_tree().change_scene_to_file("res://ScenesOfLocations/tutorial.tscn")
 
+func on_mouse_entered():
+	$start.modulate = Color.WEB_MAROON
 
-func _on_entered():
-	pass # Replace with function body.
+func on_mouse_exited():
+	$start.modulate = defaultModulate
